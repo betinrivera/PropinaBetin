5a0e1d489ae6cee74ac55934fa49290e5a643209
Maestro <Maestro@iMac-de-Maestro-4.local>
2018-08-14 18:51:42 -0700

    Cambios

--- PropinaBetin/ViewController.swift
+++ PropinaBetin/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var txtField: UITextField!
+    
+    @IBOutlet weak var Slider1: UISlider!
+    
+    @IBOutlet weak var lblResultado: UILabel!
+    
+    @IBOutlet weak var lblPorciento: UILabel!
+    
+    @IBAction func Slider1(_ sender: UISlider) {
+        
+        
+        
+        lblPorciento.text = "\(Int(Slider1.value))%"
+        
+        var op = Int(txtField.text!)! * Int(Slider1.value) / 100
+        
+        var op2 = Int(txtField.text!)! * Int(Slider1.value)
+        
+        lblPropina.text = "$\(op)"
+        
+        var resultado = op + Int(lblPropina.text)
+        
+        lblResultado.text = "$\(resultado)"
+        
+    }
+    
+    
+    @IBOutlet weak var lblPropina: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
